f6b424a2ccf6f268cf1794abb3b6e6aa11ab3b7b
Donatien <21101624@etu.unicaen.fr>
2014-01-13 11:50:16 +0100

    Ajout du css

--- src/templates/footer.php
+++ src/templates/footer.php
@@ -1,2 +1,10 @@
-	</body>
-</html>
+		</div> <!-- /container -->
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="./js/jquery-2.0.3.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+  </body>
+</html> 
+
