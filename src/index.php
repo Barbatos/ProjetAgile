a825da266c32c181b161488a6e2ab345e50b1da9
Nyakouai <asiatdragon@hotmail.fr>
2014-01-13 15:33:10 +0100

    Index
    "tattage" de terrain index, appel bdd... (et on dit "tatonnements")

--- src/index.php
+++ src/index.php
@@ -1,23 +1,34 @@
 <?php 
 require_once('includes/init.php');
 require_once('templates/header.php');
+
+$stmt = $bdd->prepare("SELECT id_trajet, nom_ville as ville_d, nom_ville as ville_a, tochar(date_trajet, 'DD/MM/YYYY') as date
+							FROM TRAJET tra 
+							JOIN VILLE vi1 ON tra.id_ville_d = vi1.id_ville
+							JOIN VILLE vi2 ON tra.id_ville_a = vi2.id_ville
+							WHERE ROWNUM < 10 ORDER BY tra.date_trajet asc");
+	$stmt->execute();
+	$data = $stmt->fetchAll(PDO::FETCH_OBJ);
+	$stmt->closeCursor();
 ?>
 
 <form name="formulaire" action="" method="post" enctype="multipart/form-data">
 	<h1> Accueil Site Co-Voiturage </h1>
 	<br/>
 	<fieldset>
-	
-		<a href="..." > <input type="button" value="Consulter les trajets disponibles"> </a>
-		<a href="..." > <input type="button" value="Ajouter un trajet"> </a>
-		<a href="..." > <input type="button" value="Gérer ses trajets"> </a>
-	</fieldset>
-	
-	<fieldset>
-		<input type="reset"/>
-		<input name="valider1" type="submit"/>
+		<legend> Derniers co-voiturage proposés </legend>
+		<select>
+		<?php
+			for ($i=0;$i < 10;$i++)
+			{
+			  echo '<option value="'.$data[$i]["id_trajet"].'">'.$data[$i]['ville_d'].' - '.$data[$i]['ville_a'].' '.$data[$i]['date'];
+			  echo '</option>';
+			}
+				
+		?>
 	</fieldset>
 </form>
   
 <?php
 require_once('templates/footer.php');
+?>
