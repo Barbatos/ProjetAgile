1c997dfb602eb44837c025f447a3d469f511b583
Charles Duprey <barbatos@urbanterror.info>
2014-01-13 22:53:04 +0100

    Fix de l'affichage de la page voirtrajet

--- src/voirtrajet.php
+++ src/voirtrajet.php
@@ -33,7 +33,8 @@ if(P()){
 }
 
 $stmt = $bdd->prepare("
-	SELECT t.*, u.*, v1.NOM_VILLE AS NOM_VILLE_D, v2.NOM_VILLE AS NOM_VILLE_A, p.ID_TRAJET AS ID_TRAJET_RESERV FROM TRAJET t
+	SELECT t.*, u.*, v1.NOM_VILLE AS NOM_VILLE_D, v2.NOM_VILLE AS NOM_VILLE_A, p.ID_TRAJET AS ID_TRAJET_RESERV, t.ID_UTILISATEUR AS USERID 
+	FROM TRAJET t
 	LEFT JOIN VILLE v1 ON v1.ID_VILLE = t.ID_VILLE_D
 	LEFT JOIN VILLE v2 ON v2.ID_VILLE = t.ID_VILLE_A
 	LEFT JOIN UTILISATEUR u ON u.ID_UTILISATEUR = t.ID_UTILISATEUR
@@ -49,45 +50,47 @@ $stmt->closeCursor();
 require_once('templates/header.php');
 ?>
 
-      <br />
-	  <strong>Informations sur le trajet <?= $data->NOM_VILLE_D ?> -> <?= $data->NOM_VILLE_A ?></strong></legend><br />
-	  
-	  Ville de départ : <?= $data->NOM_VILLE_D ?><br />
-	  Lieu de départ : <?= $data->LIEUX_D ?><br /><br />
+<div id="titre"><strong>Informations sur le trajet <?= $data->NOM_VILLE_D ?> -> <?= $data->NOM_VILLE_A ?></strong></div><br />
 
-	  Ville d'arrivée : <?= $data->NOM_VILLE_A ?><br />
-	  Lieu d'arrivée : <?= $data->LIEUX_A ?><br /><br />
+Trajet ajouté le <?= date('d/m/Y à H\hi', strtotime($data->DATE_AJOUT)) ?><br /><br />
 
-	  Date de départ : <?= $data->DATE_TRAJET ?><br />
-	  Heure de départ : <br /><br />
+Ville de départ : <?= $data->NOM_VILLE_D ?><br />
+Lieu de départ : <?= $data->LIEUX_D ?><br /><br />
 
-	  Prix : <?= $data->PRIX ?>€<br />
-	  Nombre de places : <?= $data->NB_PLACE ?><br /><br /><br />
+Ville d'arrivée : <?= $data->NOM_VILLE_A ?><br />
+Lieu d'arrivée : <?= $data->LIEUX_A ?><br /><br />
 
-	  Conducteur: <?= $data->PRENOM ?> <?= $data->NOM ?><br />
-	  Age : <?= (date('Y') - $data->DATENAISS_ANNEE) ?><br />
-      
-      <br />	
+Date de départ : <?= date('d/m/Y', strtotime($data->DATE_TRAJET)) ?><br />
+Heure de départ : <?= date('H\hi', strtotime($data->DATE_TRAJET)) ?><br /><br />
 
-      <?php 
-      if(est_connecte() && ($_SESSION['id'] != $data->ID_UTILISATEUR)){
-      	if(!empty($data->ID_TRAJET_RESERV)){
-      ?>
-      	<input type="submit" class="btn large" value="Demande de réservation confirmée. Attente du conducteur...">
-      <?php 
-        } else { 
-      ?>
-      <form name="demanderPlace" method="post" action="">
-      	<input type="submit" class="btn large" name="demanderPlace" value="Demander une place">
-      </form>
-      <?php
-        }
-      }
-      else if(!est_connecte()){
-      ?>
-      <p><a href="connexion.php" title="Se connecter">Se connecter pour réserver une place</a></p>
-      <?php 
-      }
-      ?>
+Prix : <?= $data->PRIX ?>€<br />
+Nombre de places : <?= $data->NB_PLACE ?><br /><br /><br />
+
+Conducteur: <?= $data->PRENOM ?> <?= $data->NOM ?><br />
+Age : <?= (date('Y') - $data->DATENAISS_ANNEE) ?><br />
+
+<br />	
+
+<?php 
+if(est_connecte() && ($_SESSION['id'] != $data->USERID)){
+	if(!empty($data->ID_TRAJET_RESERV)){
+	?>
+		<input type="button" class="btn large" value="Demande de réservation confirmée. Attente de la validation du conducteur...">
+	<?php 
+	} else { 
+	?>
+	<form name="demanderPlace" method="post" action="">
+		<input type="submit" class="btn large" name="demanderPlace" value="Demander une place">
+	</form>
+	<?php
+	}
+}
+else if(!est_connecte()){
+?>
+	<p><a href="connexion.php" title="Se connecter">Se connecter pour réserver une place</a></p>
+<?php 
+}
+?>
+
 <?php 
 require_once('templates/footer.php');
