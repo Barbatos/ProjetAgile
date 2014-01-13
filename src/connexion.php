fec97f62dacea4ae65b00301da1bec3c8db5531c
Charles Duprey <barbatos@urbanterror.info>
2014-01-13 11:20:32 +0100

    Ajout des pages inscription, connexion et index

---
+++ src/connexion.php
@@ -0,0 +1,30 @@
+<html>
+	<head>
+		<meta http-equiv="Content-Type" content="text/html" charset="utf-8">
+		<title>Co-voiturage: Connexion</title>
+	</head>
+	<body>
+		<form name="formulaire" action="" method="post" enctype="multipart/form-data">
+			<h1> Connexion site Co-Voiturage </h1>
+			<br/>
+			<fieldset>
+				
+				<legend> Connexion</legend>
+				<label for="identifiant"> Identifiant : </label> <br/>
+				<input id="identifiant" type="text" name="identifiant" placeholder="Votre nom d'utilisateur"><br/>
+				<br/>
+				<label for="mdp"> Mot de passe : </label> <br/>
+				<input id="mdp" type="password" name="mdp" placeholder="Votre mot de passe"><br/>
+				<br/>
+				
+				<a href="..." > <input type="button" value="Mot de passe oublié"> </a>
+				<a href="..." > <input type="button" value="Créer un compte"> </a>
+			</fieldset>
+			
+			<fieldset>
+				<input type="reset"/>
+				<input name="valider" type="submit"/>
+			</fieldset>
+		</form>
+  </body>
+</html>
---
+++ src/inscription.php
@@ -0,0 +1,26 @@
+<!-- Formulaire d'inscription  -->
+
+<form name="inscription" action="" method="post">
+	<br>
+	<fieldset>
+		<legend><b>Inscription :</b></legend>
+		Nom: 	<input type="text" name="nom" size="30" maxlength="30" ><br />
+		Prénom: <input type="text" name="prenom" size="30" maxlength="30"><br />
+		Adresse e-mail: <input type="text" name="adresseMail" size="30" maxlength="256"><br />
+		Mot de passe: <input type="text" name="motdepasse" size="30" maxlength="30"><br />
+		Année de naissance 
+		<select name="anneeNaiss">
+			<?php
+			for ($i = 1900; $i < 2014; $i++) 
+			{
+			echo '<option value="'.$i.'">'.$i.'</option>';
+			}
+			?>
+		</select>
+		<br />
+		  
+		<input type="submit" name="valider" value="Valider"> 
+
+	</fieldset>
+</form>	
+	
