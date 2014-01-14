93f1743c4793eb52d837772324a85774d47a2f05
Charles Duprey <barbatos@urbanterror.info>
2014-01-14 11:35:20 +0100

    lol derp

--- src/compte.php
+++ src/compte.php
@@ -4,6 +4,44 @@ if(!est_connecte()){
 	message_redirect('Vous devez être connecté pour voir cette page !', 'index.php');
 }
 
+if(P()){
+	if(P('modifierInfosPerso')){
+		if(P('nom') && P('prenom') && P('email') && P('tel')){
+			$stmt = $bdd->prepare('UPDATE UTILISATEUR SET NOM = :nom, PRENOM = :prenom, MAIL = :email, TEL = :tel');
+			$stmt->bindValue(':nom', P('nom'));
+			$stmt->bindValue(':prenom', P('prenom'));
+			$stmt->bindValue(':email', P('email'));
+			$stmt->bindValue(':tel', P('tel'));
+			$stmt->execute();
+			$stmt->closeCursor();
+
+			message_redirect('Vos informations personnelles ont bien été modifiées', 'compte.php');
+		}
+		else {
+			message_redirect('Vous devez renseigner tous les champs !', 'compte.php');
+		}
+	}
+
+	if(P('modifierMdp')){
+		if(P('ancienmdp') && P('nouveaumdp') && P('nouveaumdp2')){
+			if(P('nouveaumdp') != P('nouveaumdp2')){
+				message_redirect('Les deux mots de passe ne correspondent pas !', 'compte.php');
+			}
+
+			$stmt = $bdd->prepare('SELECT MDP FROM UTILISATEUR WHERE ID_UTILISATEUR = :id');
+			$stmt->bindValue(':id', $_SESSION['id']);
+			$stmt->execute();
+			$date = $stmt->fetch(PDO::FETCH_OBJ);
+			$stmt->closeCursor();
+
+			
+		}
+		else {
+			message_redirect('Vous devez renseigner tous les champs !', 'compte.php');
+		}
+	}
+}
+
 $pageActive = "Compte";
 
 require_once('templates/header.php');
@@ -22,25 +60,63 @@ $stmt->closeCursor();
 
 <div id="titre"><h1>Mon compte</h1></div>
 
-<form name="modifierCompte" action="" method="post">
+	<form name="modifierInfosPerso" action="" method="post">
       <br>
 	  <fieldset>
-	  <legend><b>Modifier votre compte :</b></legend>
-	  Nom :<input type="text" name="nom" size="30" maxlength="256" value="<?php echo $data->NOM ?>"><br/>
-	  Prénom :<input type="text" name="nomcomplet" size="30" maxlength="256" value="<?php echo $data->PRENOM ?>"><br/>
-	  Ancien mot de passe :<input type="password" name="ancienmdp" size="30" maxlength="256"><br/>
-	  Nouveau mot de passe : <input type="password" name="nouveaumdp" size="30" maxlength="256"><br/>
-	  Retaper nouveau mot de passe : <input type="password" name="nouveaumdp2" size="30" maxlength="256"><br/>
-	  Adresse e-mail :   <input type="text" name="email" size="30" maxlength="256" value = "<?php echo $data->MAIL ?>"><br/>
-	  Numero de telephone :   <input type="text" name="tel" size="30" maxlength="256" value="<?php echo $data->TEL ?>"><br/><br/>
-	
-	<legend><b>Modifier vos coordonnees bancaire :</b></legend>
-	   Numero carte bancaire :<input type="text" name="numcarte" size="30" maxlength="256" value="<?php echo $data->NUMERO_CB ?>"><br/>
-	   Date validite :<input type="text" name="datecarte" size="30" maxlength="256" value="<?php echo $data->DATE_VALIDITE?>"><br/><br/><br/>
+	  <legend><b>Modifier vos informations personnelles :</b></legend>
+	  <label>Nom</label>
+	  <div class="controls">
+	  	<input type="text" name="nom" size="30" maxlength="256" value="<?php echo $data->NOM ?>">
+	  </div><br/>
+	  <label>Prénom</label>
+	  <div class="controls">
+	  	<input type="text" name="prenom" size="30" maxlength="256" value="<?php echo $data->PRENOM ?>">
+	  </div><br/>
+	  <label>Adresse e-mail</label>
+	  <div class="controls">
+	  	<input type="text" name="email" size="30" maxlength="256" value = "<?php echo $data->MAIL ?>">
+	  </div><br/>
+	  <label>Numéro de téléphone</label>
+	  <div class="controls">
+	  	<input type="text" name="tel" size="30" maxlength="256" value="<?php echo $data->TEL ?>">
+	  </div><br/><br/>
 	  
-	  
-	  <input type="submit" name="modifier" value="Valider les modifications"> 
-      
+	  <input type="submit" name="modifier" class="btn btn-success" value="Modifier les informations personnelles"><br /><br /> 
+	</form>
+
+	<form name="modifierMdp" action="" method="post">
+	  <legend><b>Modifier votre mot de passe</b></legend>
+	  <label>Ancien mot de passe</label>
+	  <div class="controls">
+	  	<input type="password" name="ancienmdp" size="30" maxlength="256">
+	  </div><br/>
+	  <label>Nouveau mot de passe</label>
+	  <div class="controls">
+	  	<input type="password" name="nouveaumdp" size="30" maxlength="256">
+	  </div><br/>
+	  <label>Retaper nouveau mot de passe</label>
+	  <div class="controls">
+	  	<input type="password" name="nouveaumdp2" size="30" maxlength="256">
+	  </div><br/><br />
+
+	  <input type="submit" name="modifier" class="btn btn-success" value="Modifier le mot de passe"> <br /><br /> 
+	</form>
+
+	<form name="modifierCoordBancaires" action="" method="post">
+	  <legend><b>Modifier vos coordonnees bancaire :</b></legend>
+	  <label>Numero carte bancaire</label>
+	  <div class="controls">
+	  	<input type="text" name="numcarte" size="30" maxlength="256" value="<?php echo $data->NUMERO_CB ?>">
+	  </div><br/>
+	  <label>Date validite</label>
+	  <div class="controls">
+	  	<input type="text" name="datecarte" size="30" maxlength="256" value="<?php echo $data->DATE_VALIDITE ?>">
+	  </div>
+	  <br/><br/><br/>
+	 
+	  <input type="submit" name="modifier" class="btn btn-success" value="Modifier les coordonnées bancaires"> <br /><br /> 
+    </form>
+
       </br>	   
 	  </fieldset>
     </form>	
