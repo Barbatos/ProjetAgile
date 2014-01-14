d65052008e247a1c159dbda493f9d7ad138e272b
fabien14 <fabien.gouget@gmail.com>
2014-01-14 09:21:06 +0100

    formulaire de recherche de trajet

--- src/afficheTrajetRechercher.php
+++ src/afficheTrajetRechercher.php
@@ -1,3 +1,8 @@
+<?php 
+
+require_once('templates/header.php');
+?>
+
 <table>
 	<thead>
 		<tr>
@@ -12,18 +17,23 @@
 	<body>
 <?php
 	foreach($resultat as $trajet){
-		if($trajet['tr.nb_place_occupe'] < $trajet['tr.nb_place'])
+		if($trajet['NB_PLACE_OCCUPE'] < $trajet['NB_PLACE'])
 ?>
 		<tr>
 			<td><?php echo $trajet['villeDepart'];?></td>
 			<td><?php echo $trajet['villeArriver'];?></td>
-			<td><?php echo $trajet['tr.prix'];?></td>
-			<td><?php echo $trajet['tr.date_trajet'];?></td>
-			<td><?php echo $trajet['tr.nb_place'];?></td>
-			<td><a href="voirtrajet.php?id=<?php echo $trajet['tr.id'];?>" title="Detail du covoiturage">Detail</a></td>
+			<td><?php echo $trajet['PRIX'];?></td>
+			<td><?php echo $trajet['DATE_TRAJET'];?></td>
+			<td><?php echo $trajet['NB_PLACE'];?></td>
+			<td><a href="voirtrajet.php?id=<?php echo $trajet['ID_TRAJET'];?>" title="Detail du covoiturage">Detail</a></td>
 		</tr>
 <?php
 	}
 ?>
 	</body>
 </table>
+
+<?php 
+
+require_once('templates/footer.php');
+?>
---
+++ src/rechercheCovoiturage.php
@@ -0,0 +1,52 @@
+<?php
+require_once('includes/init.php');
+$erreur = '';
+$erreurBool = true;
+
+if(P('rechercheCovoiturage')){
+	if(!P('villeD')){
+		$erreur += 'Vous n\'avez pas saisi une ville de depart.\n'; 
+		$erreurBool = false;
+	}
+	if(!P('villeA')){
+		$erreur += 'Vous n\'avez pas saisi une ville d\'arriver.\n';
+		$erreurBool = false;
+	}
+	if(!P('date1')){
+	
+		$erreur += 'Vous n\'avez pas choisi de date de depart.\n';
+		$erreurBool = false;
+	}
+	if(!P('heureD')){
+	echo 'test';
+		$erreur += 'Vous n\'avez pas choisi une heure.\n';
+		$erreurBool = false;
+	}
+}
+
+$villeDepart = P('villeD');
+$villeArriver = P('villeA');
+$dateDepart = P('date1');
+$heureDepart = P('villeD');
+
+if($erreurBool && P('rechercheCovoiturage')){
+
+	$query = $bdd->prepare("SELECT tr.*,d.nom_ville as villeDepart,a.nom_ville as villeArriver from TRAJET tr join VILLE a on a.id_ville = id_ville_a join VILLE d on d.id_ville = id_ville_d where id_ville_d in (select id_ville from VILLE where nom_ville = ?) and id_ville_a in (select id_ville from VILLE where nom_ville = ?) and date_trajet=?");
+	
+	$query->execute(array($villeDepart, $villeArriver, $dateDepart));
+	$resultat = $query->fetchAll();
+	$query->closeCursor();
+}
+
+if(!P('rechercheCovoiturage') || !$erreurBool){
+	/*$query = $bdd->prepare("SELECT * from trajet order by ");
+	
+	$query->execute(array($villeDepart, $villeArriver, $dateDepart));
+	$resultat = $query->fetchAll();
+	$query->closeCursor();*/
+
+	require_once('formRechercheCovoiturage.php');
+}
+else if(P('rechercheCovoiturage')){
+	require_once('afficheTrajetRechercher.php');
+}
